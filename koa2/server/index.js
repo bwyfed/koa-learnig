a798a8c6e71613d1d883db55044a87f388db958a
bwyfed <36172659@qq.com>
2018-08-19 23:30:03 +0800

    增加session和路由相关的示例

--- koa2/server/index.js
+++ koa2/server/index.js
@@ -1,7 +1,8 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
+const session = require('koa-session')
 const app = new Koa()
-
+/*
 const mid1 = async (ctx, next) => {
     ctx.body = 'Hi'
     await next()
@@ -17,9 +18,41 @@ const mid3 = async (ctx, next) => {
     ctx.body = ctx.body + ' Luke'
 }
 
+//尾递归
+function tail (i) {
+    if (i > 3) return
+    console.log('修改前', i)
+    tail(i+1)
+    console.log('修改后', i)
+}
+tail(0)
+*/
+app.keys = ['Hi Luke'] // 用于对session进行加密
 app.use(logger())
-app.use(mid1)
-app.use(mid2)
-app.use(mid3)
+// app.use(mid1)
+// app.use(mid2)
+// app.use(mid3)
+// session示例
+app.use(session(app)) // 注意传入参数config和app
+// app.use(ctx => {
+//     if (ctx.path === '/favicon.ico') return
+
+//     let n = ctx.session.views || 0
+//     ctx.session.views = ++n // 统计访问次数
+//     ctx.body = n + ' views'
+// })
+
+// 路由示例
+app.use(ctx => {
+    if (ctx.path === '/') {
+        let n = ctx.session.views || 0
+        ctx.session.views = ++n
+        ctx.body = n + ' 次'
+    } else if (ctx.path === '/hi') {
+        ctx.body = 'Hi Luke'
+    } else {
+        ctx.body = '404'
+    }
+})
 
 app.listen(2333)
